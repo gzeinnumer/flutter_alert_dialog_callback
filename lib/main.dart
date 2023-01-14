b404fe97fd026c60500f8ea1892ad1672f1a0f8e
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2023-01-15 00:13:16 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _incrementCounter() {
+    // setState(() {
+    //   _counter++;
+    // });
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    showDialog(
+      context: context,
+      builder: (context) => ExamplesDialog(),
+    ).then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(value),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Plus Show Dialog',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ExamplesDialog extends StatelessWidget {
+  final _editTextController = TextEditingController();
+  ExamplesDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Are you sure?'),
+      content: Wrap(
+        children: [
+          TextFormField(
+            controller: _editTextController,
+            decoration: const InputDecoration(hintText: "Hint"),
+          )
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.of(context).pop('NO'),
+          child: const Text('NO'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(_editTextController.text),
+          child: const Text('YES'),
+        )
+      ],
+    );
+  }
+}
